bf625e792e15e9a82770c7f9480abb0634878686
Rogelio Schevenin Jr <rogelio@schevenin.com>
2022-03-17 23:00:14 -0700

    mostly set to start fixing implementation

--- makefile
+++ makefile
@@ -6,7 +6,7 @@ CXX=g++ -g
 # Make variable for compiler options
 #	-std=c++11  C/C++ variant to use, e.g. C++ 2011
 #	-g          include information for symbolic debugger e.g. gdb 
-CXXFLAGS=-std=c++11 -g -pthread
+CXXFLAGS=-std=c++11 -g
 
 # Rules format:
 # target : dependency1 dependency2 ... dependencyN
--- pagetable.cpp
+++ pagetable.cpp
@@ -1,33 +1,35 @@
 #include "pagetable.h"
 
-void pageInsert(PageTable *pageTable, uint32_t* address, uint32_t frame)
+void pageInsert(PageTable *pageTable, uint32_t *address, uint32_t frame)
 {
    /* get next address and process */
    if (NextAddress(pageTable->tracefile, pageTable->trace))
-   {
-      std::cout << "=================================" << std::endl;
-      for (int i = 0; i < pageTable->numLevels; i++) {
-         pageTable->virtualPageLookup = virtualAddressToPageNum(*address, pageTable->bitmask[i], pageTable->bitshift[i]) << pageTable->bitshift[i];
-         pageTable->virtualPageNumber = virtualAddressToPageNum(*address, pageTable->vpnMask, pageTable->offsetSize) << pageTable->offsetSize;
-         //pageTable->virtualPageNumber = virtualAddressToPageNum(*address, pageTable->bitmask[i], pageTable->bitshift[i]);
-         //pageTable->offset = virtualAddressToPageNum(address, pageTable->bitmask[i], (pageTable->bitshift[i]));
-         //fprintf(stdout, "\nLevel: %i\n", i);
-         fprintf(stdout, "Virtual Address: %08X\n", pageTable->trace->addr);
+   {  
+      pageTable->virtualPageNumber = virtualAddressToPageNum(*address, pageTable->vpnMask, pageTable->offsetSize) << pageTable->offsetSize;
+      pageTable->offset = virtualAddressToPageNum(*address, pageTable->offsetMask, pageTable->offsetSize) << pageTable->offsetSize;
 
-        fprintf(stdout, "\nOffset mask: %08X\n", pageTable->offsetMask);
-         fprintf(stdout, "VP Lookup Mask (%i): %08X\n", i, pageTable->bitmask[i]);
-         fprintf(stdout, "\nVPN Mask (%i): %X\n", i, pageTable->vpnMask);
-         fprintf(stdout, "\nVP Lookup (%i): %08X\n", i, pageTable->virtualPageLookup);
-         fprintf(stdout, "\nVPN (%i): %X\n", i, pageTable->virtualPageNumber);
-        fprintf(stdout, "Offset (%i): %X\n", i, pageTable->offset);
-         std::cout << std::endl;
+      std::cout << "=================================" << std::endl;
+      fprintf(stdout, "Virtual Address: %08X\n", pageTable->trace->addr);
+      fprintf(stdout, "VPN Mask: %X\n", pageTable->vpnMask);
+      fprintf(stdout, "Offset mask: %08X\n", pageTable->offsetMask);
+      fprintf(stdout, "VPN: %08X\n", pageTable->virtualPageNumber);
+      fprintf(stdout, "Offset: %08X\n", pageTable->offset);
+
+      for (int i = 0; i < pageTable->numLevels; i++)
+      {
+         pageTable->virtualPageLookup = virtualAddressToPageNum(*address, pageTable->bitmask[i], pageTable->bitshift[i]) << pageTable->bitshift[i];
+         fprintf(stdout, "\nLevel: %i\n", i);
+         fprintf(stdout, "Virtual page lookup mask (%i): %08X\n", i, pageTable->bitmask[i]);
+         fprintf(stdout, "Virtual page lookup (%i): %08X\n", i, pageTable->virtualPageLookup);
       }
+
    }
 
    pageTable->instructionsProcessed++; // ensures correct amount of addreesses are processed
 }
 
-uint32_t virtualAddressToPageNum(uint32_t virtualAddress, uint32_t mask, uint32_t shift) {
+uint32_t virtualAddressToPageNum(uint32_t virtualAddress, uint32_t mask, uint32_t shift)
+{
    uint32_t result;
    result = mask & virtualAddress;
    result = result >> shift;
